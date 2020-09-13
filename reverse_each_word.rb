bb5eb12166a1a4ab2acd6af885bc611fde526eee
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-09-13 01:57:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
-reverse_each_word(sentence)
+def reverse_each_word(sentence)
  array = sentence.split(" ")
  new_array = []
  array.each do |word|
    new_array << word.reverse
  end
- puts new_array.join(" ")
- end 
+ puts new_array.join(" ") 
+end 
  
  
  
